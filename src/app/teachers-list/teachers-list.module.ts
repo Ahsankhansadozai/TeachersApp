f767b7673f2b9909c0ddc69f571c5d0a4cd8b5b6
unknown <ahsan.bsse3200@iiu.edu.pk>
2019-10-29 01:56:54 -0700

    initial commit

---
+++ src/app/teachers-list/teachers-list.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TeachersListPage } from './teachers-list.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TeachersListPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TeachersListPage]
+})
+export class TeachersListPageModule {}
